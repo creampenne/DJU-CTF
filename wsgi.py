7107c2e1b8bfef28439b584c78ab2deb801c8611
WonHee Yoon <wonhee5757@gmail.com>
2023-06-12 18:41:05 +0900

    last edit

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,4 @@ from CTFd import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host="0.0.0.0", port=40004)
+    app.run(threaded=True, host="0.0.0.0", port=80)
